29ed700b4e7fef991b7bdcfb8ca60e5beeb6e6bc
jdaev <jdhn42@gmail.com>
2021-05-15 11:47:13 -0400

    tasK: Separated drives into public and private

--- src/drives.ts
+++ src/drives.ts
@@ -1,19 +1,12 @@
 import * as arweave from './arweave';
-import * as arweavePrivate from './arweave_private';
 import { TransactionUploader } from 'arweave/node/lib/transaction-uploader';
 
-import { ArDriveUser, ArFSEncryptedData } from './types/base_Types';
-import { ArFSLocalFile, ArFSLocalDriveEntity, ArFSLocalPrivateDriveEntity } from './types/client_Types';
+import { ArDriveUser } from './types/base_Types';
+import { ArFSLocalFile, ArFSLocalDriveEntity } from './types/client_Types';
 import { newArFSFileMetaData } from './arfs';
 import { v4 as uuidv4 } from 'uuid';
-import { deriveDriveKey, driveEncrypt } from './crypto';
 import { JWKInterface } from 'arweave/node/lib/wallet';
-
-export const appName = 'ArDrive-Desktop';
-export const webAppName = 'ArDrive-Web';
-export const appVersion = '0.1.0';
-export const arFSVersion = '0.11';
-export const cipher = 'AES256-GCM';
+import { appName, appVersion, arFSVersion } from './constants';
 
 // Creates an new Drive transaction and uploader using ArFS Metadata
 export async function newArFSDriveMetaData(
@@ -34,51 +27,6 @@ export async function newArFSDriveMetaData(
 		console.log('Creating a new Public Drive (name: %s) on the Permaweb', driveMetaData.entity.name);
 		const transaction = await arweave.createDriveTransaction(
 			driveMetaDataJSON,
-			driveMetaData.entity,
-			walletPrivateKey
-		);
-
-		// Update the file's data transaction ID
-		driveMetaData.entity.txId = transaction.id;
-
-		// Create the File Uploader object
-		const uploader = await arweave.createDataUploader(transaction);
-
-		return { driveMetaData, uploader };
-	} catch (err) {
-		console.log(err);
-		console.log('Error creating new ArFS Drive transaction and uploader %s', driveMetaData.entity.name);
-		return null;
-	}
-}
-
-export async function newArFSPrivateDriveMetaData(
-	user: ArDriveUser,
-	walletPrivateKey: JWKInterface,
-	driveMetaData: ArFSLocalPrivateDriveEntity
-): Promise<{ driveMetaData: ArFSLocalPrivateDriveEntity; uploader: TransactionUploader } | null> {
-	try {
-		// Create a JSON file, containing necessary drive metadata
-		const driveMetaDataTags = {
-			name: driveMetaData.entity.name,
-			rootFolderId: driveMetaData.entity.rootFolderId
-		};
-
-		// Convert to JSON string
-		const driveMetaDataJSON = JSON.stringify(driveMetaDataTags);
-
-		// Check if the drive is public or private
-		console.log('Creating a new Private Drive (name: %s) on the Permaweb', driveMetaData.entity.name);
-		const driveKey: Buffer = await deriveDriveKey(
-			user.dataProtectionKey,
-			driveMetaData.entity.driveId,
-			user.walletPrivateKey
-		);
-		const encryptedDriveMetaData: ArFSEncryptedData = await driveEncrypt(driveKey, Buffer.from(driveMetaDataJSON));
-		driveMetaData.entity.cipher = encryptedDriveMetaData.cipher;
-		driveMetaData.entity.cipherIV = encryptedDriveMetaData.cipherIV;
-		const transaction = await arweavePrivate.createPrivateDriveTransaction(
-			encryptedDriveMetaData.data,
 			driveMetaData.entity,
 			walletPrivateKey
 		);
@@ -128,36 +76,7 @@ export async function newArFSDrive(driveName: string, login: string): Promise<Ar
 
 	return drive;
 }
-export async function newArFSPrivateDrive(driveName: string, login?: string): Promise<ArFSLocalDriveEntity> {
-	const driveId = uuidv4();
-	const rootFolderId = uuidv4();
-	const unixTime = Math.round(Date.now() / 1000);
-	console.log('Creating a new private drive %s | %s', driveName, driveId);
-	const drive: ArFSLocalPrivateDriveEntity = {
-		id: 0,
-		owner: login != undefined ? login : '',
-		isLocal: 1,
-		entity: {
-			appName: appName,
-			appVersion: appVersion,
-			arFS: arFSVersion,
-			contentType: '',
-			driveId: driveId,
-			drivePrivacy: 'personal',
-			rootFolderId: rootFolderId,
-			syncStatus: 0,
-			txId: '0',
-			unixTime: unixTime,
-			name: '',
-			entityType: '',
-			cipher: cipher,
-			cipherIV: '',
-			driveAuthMode: 'password'
-		}
-	};
 
-	return drive;
-}
 // This will create and upload a new drive entity and its root folder
 export async function createAndUploadArFSDriveAndRootFolder(
 	user: ArDriveUser,
