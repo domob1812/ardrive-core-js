14e8404fbc2711b9d9f70dc2a9fffdda905e3c38
Philip Mataras <vilenarios@gmail.com>
2021-05-25 22:45:52 -0400

    Adding notes for each function

--- src/db/db_get.ts
+++ src/db/db_get.ts
@@ -269,75 +269,136 @@ export const getNewDrivesFromDriveTable = (login: string) => {
 	return all(`SELECT * FROM Drive WHERE login = ? AND metaDataTxId = '0'`, [login]);
 };
 
+// YES
+// getAllLocalFiles, getAllLocalPrivateFiles, getAllLocalFolders, getAllLocalPrivateFolders
+// Used in profile.ts in order to get all files and folders
+// Split into public/private and files/folders.  Returns arrays of each.
 export const getAllFilesByLoginFromSyncTable = (login: string) => {
 	return all(`SELECT * FROM Sync WHERE login = ? ORDER BY unixTime DESC`, [login]);
 };
 
+// YES
+// getFilesToUpload, getPrivateFilesToUpload, getFoldersToUpload, getPrivateFoldersToUpload
+// Used in bundles.ts, node.ts and arweave.ts, gets all queued files for upload for a user, using several parameters
+// syncStatus on the FileData type has to be 1 in order to indicate a file is ready for upload
+// syncStatus on the Entity type has to be 1 in order to indicate the metadata is ready for upload
+// isLocal also has to be false, indicating that we have to download this file
+// Returns all local Public/Private files to be downloaded in an array
 export const getFilesToUploadFromSyncTable = (login: string) => {
 	return all(`SELECT * FROM Sync WHERE (login = ?) AND (fileDataSyncStatus = 1 OR fileMetaDataSyncStatus = 1)`, [
 		login
 	]);
 };
 
+// YES
+// getAllRecentlyUploadedBundles
+// Used in node.ts and gets all bundles for a user that have been uploaded to the arweave network, but not confirmed yet (syncStatus = 2)
+// Returns all ARFS Bundles in an array
 export const getAllUploadedBundlesFromBundleTable = (login: string) => {
 	return all(`SELECT * FROM Bundle WHERE (login = ?) AND (bundleSyncStatus = 2)`, [login]);
 };
 
+// NO
+// Can be replaced with getFilesToUpload, getPrivateFilesToUpload, getFoldersToUpload, getPrivateFoldersToUpload
 export const getAllUploadedDataItemsFromSyncTable = (login: string, bundleTxId: string) => {
 	return all(`SELECT * FROM Sync WHERE login = ? AND bundleTxId = ?`, [login, bundleTxId]);
 };
 
+// YES
+// getRecentlyUploadedFiles, getRecentlyUploadedPrivateFiles, getRecentlyUploadedFolders, getRecentlyUploadedPrivateFolders
+// Used in node.ts, gets all queued files for upload for a user, using several parameters
+// syncStatus on the FileData type has to be 2 in order to indicate a file data has recently been uploaded
+// syncStatus on the Entity type has to be 2 in order to indicate the metadata has recently been uploaded
+// Returns all local Public/Private files that have been recently uploaded in an array
 export const getAllUploadedFilesFromSyncTable = (login: string) => {
 	return all(`SELECT * FROM Sync WHERE (login = ?) AND (fileDataSyncStatus = 2 OR fileMetaDataSyncStatus = 2)`, [
 		login
 	]);
 };
 
+// NO
+// File Conflict management has to be reworked
 export const getMyFileDownloadConflicts = (login: string) => {
 	return all(`SELECT * FROM Sync WHERE isLocal = 2 AND login = ?`, [login]);
 };
 
+// YES
+// getFilesWithMissingPaths, getPrivateFilesWithMissingPaths, getFoldersWithMissingPaths, getPrivateFoldersWithMissingPaths
+// Used in common.ts to find and set any file or folder with an empty path
+// does this need to be split into public/private files and folders?
 export const getAllMissingPathsFromSyncTable = () => {
 	return all(`SELECT * FROM Sync WHERE filePath = '' ORDER BY id DESC`);
 };
 
+// YES
+// getFilesWithMissingParents, getPrivateFilesWithMissingParents, getFoldersWithMissingParents, getPrivateFoldersWithMissingParents
+// Used in common.ts to set all parent folder IDs that are missing.  This is used in order to add parent folder IDs to recently found files/folders in files.ts.
+// split into public/private files and folders?
 export const getAllMissingParentFolderIdsFromSyncTable = () => {
 	return all(`SELECT * FROM Sync WHERE parentFolderId = ''`);
 };
 
+// YES
+// getLocalFolders, getLocalPrivateFolders
+// Used in common.ts to perform functions to get and set all local folder hashes and sizes, returns Local Public/Private folder types in array
+// split into public/private folders
 export const getAllLocalFoldersFromSyncTable = () => {
 	return all(`SELECT * FROM Sync WHERE entityType = 'folder' AND isLocal = 1`);
 };
 
+// NO
+// Not used anywhere
 export const getAllLocalFilesFromSyncTable = () => {
 	return all(`SELECT * FROM Sync WHERE entityType = 'file' AND isLocal = 1`);
 };
 
+// NO
+// Not used anywhere
 export const getAllLocalFilesAndFoldersFromSyncTable = () => {
 	return all(`SELECT * FROM Sync WHERE entityType = 'file' AND entityType = 'folder' AND isLocal = 1`);
 };
 
+// YES
+// getUnhashedLocalFiles, getUnhashedLocalPrivateFiles
+// Used in common.ts to set the file hashes of local files that have been found in files.ts
+// Returns Local Public/Private file types in array
 export const getAllUnhashedLocalFilesFromSyncTable = () => {
 	return all(`SELECT * FROM Sync WHERE fileHash = '' AND entityType = 'file' AND isLocal = 1`);
 };
 
-// Gets all files that are not Cloud Only so they can be validated they still exist locally
+// YES
+// getLatestLocalFiles, getLatestLocalPrivateFiles, getLatestLocalFolders, getLatestLocalPrivateFolders
+// Used in common.ts and gets all files that are not Cloud Only so they can be validated they still exist locally
+// If the file does not exist locally, its syncStatus will be set to 1 and it will be redownloaded
+// returns local public/private file/folder types in array
 export const getAllLatestFileAndFolderVersionsFromSyncTable = () => {
 	return all(`SELECT * FROM Sync WHERE cloudOnly = 0 AND isLocal = 1`);
 };
 
+// NO
+// Not used anywhere
 export const getAllFromProfile = () => {
 	return all(`SELECT * FROM Profile`);
 };
 
+// NO
+// Not used anywhere
 export const getAllDrivesFromDriveTable = () => {
 	return all(`SELECT * FROM Drive`);
 };
 
+// YES
+// getLocalDrives, getLocalPrivateDrives
+// Returns all public/private drives as an array for a given owner aka arweave public key
+// Must also include driveSharing flags
 export const getAllDrivesByLoginFromDriveTable = (login: string) => {
 	return all(`SELECT * FROM Drive WHERE login = ? AND isLocal = 1`, [login]);
 };
 
+// YES
+// getUnsyncedDrives, getUnsyncedPrivateDrives
+// Used in prompts.ts in CLI and returns all public/private drives as an array for a given owner aka arweave public key
+// Will only return drives that are not flagge as being synced locally with isLocal = 1
 export const getAllUnSyncedPersonalDrivesByLoginFromDriveTable = (login: string, drivePrivacy: string) => {
 	return all(
 		`SELECT * FROM Drive WHERE login = ? AND drivePrivacy = ? AND driveSharing = 'personal' AND isLocal != 1 AND driveName != 'Invalid Drive Password'`,
@@ -345,10 +406,14 @@ export const getAllUnSyncedPersonalDrivesByLoginFromDriveTable = (login: string,
 	);
 };
 
+// NO
+// Reuse getLocalDrives, getLocalPrivateDrives
 export const getAllPersonalDrivesByLoginFromDriveTable = (login: string) => {
 	return all(`SELECT * FROM Drive WHERE login = ? AND driveSharing = 'personal' AND isLocal = 1`, [login]);
 };
 
+// NO
+// Reuse getLocalDrives, getLocalPrivateDrives
 export const getAllDrivesByPrivacyFromDriveTable = (login: string, driveSharing: string, drivePrivacy: string) => {
 	return all(`SELECT * FROM Drive WHERE login = ? AND driveSharing = ? AND drivePrivacy = ? AND isLocal = 1`, [
 		login,
